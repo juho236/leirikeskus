7f02d2a59f18b9c35e5bb2d23c2ad4b21372cc07
juho236 <juho.toivola@edu.tampere.fi>
2023-11-03 09:25:19 +0200

    c

---
+++ main.js
@@ -0,0 +1,15 @@
+const popup = (id,msg) => {
+    const target = document.getElementById(id)
+    let el = document.getElementById(id+"popup")
+    if (el) {
+        el.remove()
+    } else {
+        el = document.createElement("div")
+        el.classList = "popup"
+        el.id = id+"popup"
+        el.innerHTML = msg
+        el.style.top = (target.offsetTop+target.parentElement.offsetTop)+"px"
+        el.style.left = (target.offsetLeft+target.parentElement.offsetLeft)+"px"
+        document.body.appendChild(el)
+    }
+} 
