dc66f9b420fc7f796d0e2d23ea1c5cec6bc4a4cc
juho236 <juho.toivola@edu.tampere.fi>
2023-11-24 08:34:17 +0200

    edits

--- main.js
+++ main.js
@@ -15,22 +15,47 @@ const popup = (id,msg) => {
 } 
 
 const runForm = () => {
-    const id = document.URL.charAt(document.URL.length-1)
+    let id = Math.min(Number(document.URL.charAt(document.URL.length-1)),4)
+    if (!id) {
+        id = 0
+    }
+    let selected = id
     const options = ["Pieni Mökki","Suuri Mökki","Teltta","Asuntoautopaikka","Hotellihuone"]
     const dropdownframe = document.createElement("div")
     dropdownframe.classList = "dropdown"
     const input = document.createElement("input")
     input.type = "button"
     input.onclick = () => {
-        if (dropdownframe.style.display == "none") {
-            dropdownframe.style.display = ""
-        } else {
+        if (dropdownframe.style.display != "none") {
             dropdownframe.style.display = "none"
+            select(selected)
+        } else {
+            dropdownframe.style.display = ""
+            input.value = "Valitse joku näistä"
         }
     }
-    const select = (id) => {
+    const frames = []
 
+    for (let i=0;i<options.length;i++) {
+        const fr = document.createElement("input")
+
+        fr.type = "button"
+        fr.value = options[i]
+        fr.onclick = () => {
+            select(i)
+        }
+
+        dropdownframe.appendChild(fr)
+        frames[i] = fr
+    }
+
+    const select = (id) => {
+        selected = id
+        dropdownframe.style.display = "none"
+        input.value = options[id]
     }
     select(id)
-    
+    const fr = document.getElementById("frame")
+    fr.appendChild(input)
+    fr.appendChild(dropdownframe)
 }
